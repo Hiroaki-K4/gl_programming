3a25cc8d8be4d22ed4953d053c0d5b780b522721
Hiroaki-K4 <49no19@gmail.com>
2024-01-02 11:42:56 +0900

    Fix reflection

--- srcs/chapter_13/specular/colors.fs
+++ srcs/chapter_13/specular/colors.fs
@@ -19,11 +19,10 @@ void main() {
 
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = refrect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
-    // vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
 }
